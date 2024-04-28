46df0d0254ef9509a6a14f583ca77b51ca7c09e8
EzekielCz <ezechielcazacu@gmail.com>
2024-04-27 19:10:45 -0500

    Continued update

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -233,7 +233,7 @@ const Register = () => {
                         />
                         <p id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
                             8 to 24 characters.<br />
-                              Must include uppercase and lowercase letters, a number and a special character.<br />
+                              Must include uppercase and lowercase letters.<br />
                             Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
                         </p>
 
