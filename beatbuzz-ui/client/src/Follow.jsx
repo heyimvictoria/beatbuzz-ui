44f63cc8ab22f6ace1b6a7386f3c2061d574f647
EzekielCz <ezechielcazacu@gmail.com>
2024-05-09 04:07:38 -0500

    fixed follow post request

--- beatbuzz-ui/client/src/Follow.jsx
+++ beatbuzz-ui/client/src/Follow.jsx
@@ -1,47 +1,50 @@
-// import { useEffect, useState } from "react";
-// import axios from "./api/axios";
+import { useEffect, useState } from "react";
+import axios from "./api/axios";
 
-// const Follow = () => {
-//     const FOLLOW_URL = "http://localhost:8080/api/follow/follow";
-//     // const queryParams = new URLSearchParams(window.location.search)
-//     const currentUser = localStorage.getItem("username");
+const Follow = () => {
+    const FOLLOW_URL = "http://localhost:8080/api/follow/follow";
+    const username =localStorage.getItem('username');
+        const postUser= localStorage.getItem("postUser");
+        const payload = {
+            username, postUser
+        }
+    // const queryParams = new URLSearchParams(window.location.search)
+    // const currentUser = localStorage.getItem("username");
 
-//     /*
+    /*
 
-//     user
+    user
+*/
 
-
-//     */
-
-
-//     const handleSubmit = async (e) => {
-//         e.preventDefault();
-
-//         const payload = {
-//             username, userId
-//         }
-
-//         useEffect(() => {
-//             const token = JSON.parse(localStorage.getItem('user')).accessToken;
-//             const AuthString = 'Bearer '.concat(token);
-//             const response = axios.post(FOLLOW_URL,
-//                 JSON.stringify({ username, }),
-//                 payload,
-//                 {
-//                     headers: {
-//                         accept: "*/*",
-//                         "Content-Type": "application/json",
-//                         Authorization: AuthString,
-//                     }
-//                 }
-//             )
-//         }
-//         );
-//     }
-// }
-
-// // }
+    
+    useEffect(() => {
+        // const username = JSON.parse(localStorage.getItem('username')).accessToken;
+       
+        const AuthString = 'Bearer '.concat(username);
+        const response = axios.post(FOLLOW_URL,
+            // JSON.stringify({ username, }),
+            payload,
+            {
+                headers: {
+                    accept: "*/*",
+                    "Content-Type": "application/json",
+                    Authorization: AuthString,
+                }
+                // response.data 
+            }
+        )
+    }
+    
 
 
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
 
-// export default Follow;
+
+        
+
+        );
+    }
+
+
+export default Follow;
